0b03d9c38ab1163b84e093fad768a579d2a038a5
naijun0403 <naijun0403@gmail.com>
2023-10-31 01:53:56 +0900

    feat: global log summary

--- app/src/main/kotlin/app/msgbot/ui/pages/homeLayout.kt
+++ app/src/main/kotlin/app/msgbot/ui/pages/homeLayout.kt
@@ -11,10 +11,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -47,6 +51,8 @@ fun HomeLayout() {
         Spacer(modifier = Modifier.padding(10.dp))
 
         RecentVersionCard()
+
+        GlobalLogSummary()
     }
 }
 
@@ -91,12 +97,14 @@ fun AllEnabledCard() {
 
 /**
  * TODO: 최신버전이 아니면 에러 메시지를 보여주어야 함 (일단 무조건 최신버전으로 가정)
+ *
+ * issue: 기기마다 비율이 다름
  */
 @Composable
 fun RecentVersionCard() {
     Card(
         modifier = Modifier
-            .size(
+            .requiredSize(
                 width = 330.dp,
                 height = 110.dp
             ),
@@ -135,6 +143,66 @@ fun RecentVersionCard() {
 
                     Text(text = "현재 버전 정보: 1.0")
                 }
+            }
+        }
+    }
+}
+
+/**
+ * TODO: 글로벌 로그 요약 (내용 미구현)
+ */
+@Composable
+fun GlobalLogSummary() {
+    Card(
+        modifier = Modifier
+            .width(350.dp)
+            .height(300.dp)
+            .padding(top = 30.dp, start = 10.dp, end = 10.dp, bottom = 10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(start = 20.dp, top = 15.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = "글로벌 로그", style = MaterialTheme.typography.titleLarge)
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = 20.dp, top = 15.dp, end = 20.dp, bottom = 15.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(130.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.inverseOnSurface,
+                )
+            ) {
+                // TODO: 글로벌 로그 요약 내용
+            }
+        }
+
+        // detail button
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, top = 15.dp, end = 20.dp, bottom = 15.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.End
+        ) {
+            Button(
+                modifier = Modifier.requiredHeight(54.dp)
+                    .padding(bottom = 14.dp),
+                onClick = { /* TODO */ }
+            ) {
+                Text(text = "자세히 보기")
             }
         }
     }
