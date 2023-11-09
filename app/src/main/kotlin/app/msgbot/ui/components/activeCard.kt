6d107d551ae8e71b1db3d7e51ea183bffe11537b
naijun0403 <naijun0403@gmail.com>
2023-11-09 15:20:16 +0900

    feat: add switch onClick

--- app/src/main/kotlin/app/msgbot/ui/components/activeCard.kt
+++ app/src/main/kotlin/app/msgbot/ui/components/activeCard.kt
@@ -2,24 +2,24 @@ package app.msgbot.ui.components
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import app.msgbot.ui.font.D2CodingRegular
 import app.msgbot.ui.font.SpoqaHanSansNeoRegular
 
 /**
@@ -27,6 +27,10 @@ import app.msgbot.ui.font.SpoqaHanSansNeoRegular
  */
 @Composable
 fun ActiveCard() {
+    var enabled by remember {
+        mutableStateOf(true)
+    }
+
     Card(
         modifier = Modifier
             .width(330.dp)
@@ -63,8 +67,10 @@ fun ActiveCard() {
                     .padding(end = 20.dp)
             ) {
                 Switch(
-                    checked = true,
-                    onCheckedChange = {},
+                    checked = enabled,
+                    onCheckedChange = {
+                        enabled = it
+                    },
                 )
             }
         }
