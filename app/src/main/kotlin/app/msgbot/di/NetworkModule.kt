2a13f107907b38cf77519eb3a6de9e24600a3801
naijun0403 <naijun0403@gmail.com>
2023-11-11 01:56:10 +0900

    feat: register github service

--- app/src/main/kotlin/app/msgbot/di/NetworkModule.kt
+++ app/src/main/kotlin/app/msgbot/di/NetworkModule.kt
@@ -1,12 +1,16 @@
 package app.msgbot.di
 
 import android.content.Context
+import app.msgbot.network.service.GithubApiService
+import de.jensklingenberg.ktorfit.Ktorfit
+import de.jensklingenberg.ktorfit.ktorfit
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.android.ext.koin.androidContext
+import org.koin.compose.koinInject
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
@@ -26,4 +30,21 @@ val httpModule = module {
         provideHttpClient(androidContext(), get())
     }
     singleOf(::provideJson)
+}
+
+val ktorfitModule = module {
+    fun provideKtorfit(
+        httpModule: HttpClient
+    ) = ktorfit {
+        baseUrl(GithubApiService.baseUrl)
+        httpClient(httpModule)
+    }
+
+    single<Ktorfit> {
+        provideKtorfit(get())
+    }
+
+    single<GithubApiService> {
+        get<Ktorfit>().create()
+    }
 }
