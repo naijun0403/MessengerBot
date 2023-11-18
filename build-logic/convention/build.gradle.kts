e6fa643344f86709df73082cf948c0ee93ac95fd
naijun0403 <naijun0403@gmail.com>
2023-11-19 01:50:47 +0900

    feat: add build-logic

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,61 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "app.msgbot.convention"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "msgbot.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "msgbot.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "msgbot.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("room") {
+            id = "room"
+            implementationClass = "library.RoomPlugin"
+        }
+        register("appModule") {
+            id = "app"
+            implementationClass = "module.AppPlugin"
+        }
+        register("coreModule") {
+            id = "core"
+            implementationClass = "module.CorePlugin"
+        }
+        register("androidLint") {
+            id = "msgbot.android.lint"
+            implementationClass = "AndroidLintConventionPlugin"
+        }
+        register("jvmLibrary") {
+            id = "msgbot.jvm.library"
+            implementationClass = "JvmLibraryConventionPlugin"
+        }
+    }
+}
---
+++ build-logic/convention/src/main/kotlin/module/AppPlugin.kt
@@ -0,0 +1,40 @@
+package module
+
+import app.msgbot.convention.util.androidTestImplementation
+import app.msgbot.convention.util.debugImplementation
+import app.msgbot.convention.util.implementation
+import app.msgbot.convention.util.library
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.project
+
+class AppPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("msgbot.android.application.compose")
+            }
+
+            dependencies {
+                implementation(project(":core"))
+                implementation(library("core.ktx"))
+                implementation(library("lifecycle.runtime.ktx"))
+                implementation(library("activity.compose"))
+                implementation(platform(library("compose.bom")))
+                implementation(library("ui"))
+                implementation(library("ui.graphics"))
+                implementation(library("ui.tooling.preview"))
+                implementation(library("material3"))
+                androidTestImplementation(library("androidx.test.ext.junit"))
+                androidTestImplementation(library("espresso.core"))
+                androidTestImplementation(platform(library("compose.bom")))
+                androidTestImplementation(library("ui.test.junit4"))
+                androidTestImplementation(library("navigation.testing"))
+                androidTestImplementation(library("hilt.navigation"))
+                debugImplementation(library("ui.tooling"))
+                debugImplementation(library("ui.test.manifest"))
+            }
+        }
+    }
+}
